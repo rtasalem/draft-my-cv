adbe57d18ca0a8a558ce33f33672d0660a06222f
Rana Salem <ranatasalem@gmail.com>
2024-12-31 01:17:23 +0000

    configure routes cont.

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/submit', (req, res) => {
+router.get('/', (req, res) => {
   res.send('submit your details here')
 })
 
--- app/server.js
+++ app/server.js
@@ -10,17 +10,13 @@ const server = async () => {
   const app = express()
   const port = 3000
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
   app.use(healthy)
   app.use(healthz)
   app.use(submit)
   app.use(download)
 
   app.listen(port, () => {
-    console.log(`Application listening on port ${port}`)
+    console.log(`Application listening on http://localhost:${port}`)
   })
 }
 
