fa48dedc789e6c91acee315d315cd6deb5ffbcf9
Rana Salem <ranatasalem@gmail.com>
2024-12-31 02:11:21 +0000

    resovle merge conflicts from main branch

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+import healthy from './healthy.js'
+import healthz from './healthz.js'
+import submit from './submit.js'
+import download from './download.js'
+
+export {
+  healthy,
+  healthz,
+  submit,
+  download
+}
---
+++ app/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import nunjucks from 'nunjucks'
+import {
+  healthy,
+  healthz,
+  submit,
+  download
+} from './routes/index.js'
+
+const server = async () => {
+  const app = express()
+  const port = 3000
+
+  nunjucks.configure('app/views', {
+    autoescape: true,
+    express: app
+  })
+
+  app.set('view engine', 'njk')
+
+  app.use(healthy)
+  app.use(healthz)
+  app.use(submit)
+  app.use(download)
+
+  app.listen(port, () => {
+    console.log(`Application listening on http://localhost:${port}`)
+  })
+}
+
+export { server }
