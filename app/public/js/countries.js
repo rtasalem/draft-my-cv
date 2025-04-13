26534a981371c2c868cbaa84db62af906a3f83f7
Rana Salem <ranatasalem@gmail.com>
2025-04-13 23:14:31 +0100

    add placeholder/default option for countries & year drop downs

--- app/public/js/countries.js
+++ app/public/js/countries.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://restcountries.com/v3.1/all').then(res => {
     return res.json()
   }).then(countries => {
-    let output = ''
+    let options = '<option value="" disabled selected>Country</option>'
 
     countries
       .sort((a, b) => a.name.common.localeCompare(b.name.common))
       .forEach(country => {
-        output += `<option>${country.name.common}</option>`
+        options += `<option value="${country.name.common}">${country.name.common}</option>`
       })
 
-    selectDrop.innerHTML = output
+    selectDrop.innerHTML = options
   }).catch(err => {
     console.log(err)
   })
--- app/public/js/year.js
+++ app/public/js/year.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const selectDrop = document.getElementById('ug-graduation-year')
+  const selects = document.querySelectorAll('.graduation-year')
+  const currentYear = new Date().getFullYear()
 
-  let currentYear = new Date().getFullYear()
-  const earliestYear = 1900
-  while (currentYear >= earliestYear) {
-    const dateOption = document.createElement('option')
-    dateOption.text = currentYear
-    dateOption.value = currentYear
-    selectDrop.add(dateOption)
-    currentYear -= 1
-  }
+  selects.forEach(select => {
+    select.innerHTML = '<option value="" disabled selected>Year</option>'
+
+    for (let year = currentYear; year >= 1900; year--) {
+      select.innerHTML += `<option value="${year}">${year}</option>`
+    }
+  })
 })
